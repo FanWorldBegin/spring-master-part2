08aaf4f2130aec9e6fa842a4396f8dc9411025af
FanWorldBegin <ericaFront1995@gmail.com>
2022-10-04 20:48:15 +0800

    machers

--- src/main/java/springboot/config/ProjectSecurityConfig.java
+++ src/main/java/springboot/config/ProjectSecurityConfig.java
@@ -6,21 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class ProjectSecurityConfig  /*extends WebSecurityConfigurerAdapter*/  {
-
-    /*@Override
-    protected void configure(HttpSecurity http) throws Exception {
-        // Permit All Requests inside the Web Application
-        http.authorizeRequests().
-                    anyRequest().permitAll().
-                    and().formLogin()
-                    .and().httpBasic();
-        // Deny All Requests inside the Web Application
-        *//*http.authorizeRequests().
-                anyRequest().denyAll().
-                and().formLogin()
-                .and().httpBasic();*//*
-    }*/
+public class ProjectSecurityConfig  {
 
     /**
      * From Spring Security 5.7, the WebSecurityConfigurerAdapter is deprecated to encourage users
@@ -32,19 +18,16 @@ public class ProjectSecurityConfig  /*extends WebSecurityConfigurerAdapter*/  {
      */
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-
-        // Permit All Requests inside the Web Application
-//        http.authorizeRequests().anyRequest().permitAll().
-//                and().formLogin()
-//                .and().httpBasic();
-
-        // Deny All Requests inside the Web Application
-        http.authorizeRequests().anyRequest().denyAll().
-                    and().formLogin()
-                    .and().httpBasic();
-
+        http.csrf().disable()
+                .authorizeRequests()
+                .mvcMatchers("/home").permitAll()
+                .mvcMatchers("/holidays/**").permitAll()
+                .mvcMatchers("/contact").permitAll()
+                .mvcMatchers("/saveMsg").permitAll()
+                .mvcMatchers("/courses").authenticated()
+                .mvcMatchers("/about").permitAll()
+                .and().formLogin().and().httpBasic();
         return http.build();
-
     }
 
 }
