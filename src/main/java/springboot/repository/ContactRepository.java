557678307b3129f9c6c89bb77256b9c6d0442c83
FanWorldBegin <ericaFront1995@gmail.com>
2022-11-20 13:13:06 +0800

    section27-customQueries

--- src/main/java/springboot/repository/ContactRepository.java
+++ src/main/java/springboot/repository/ContactRepository.java
@@ -2,8 +2,12 @@ package springboot.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import springboot.model.Contact;
 
 import java.util.List;
@@ -17,7 +21,32 @@ DB related operations and
 public interface ContactRepository extends CrudRepository<Contact, Integer> {
 
     List<Contact> findByStatus(String status);
-    // 分页 Page 类型
-    Page<Contact> findByStatus(String status, Pageable pageable);
+
+    // 参数可以 ?1 也可以直接使用变量名status, 此时不会按照方法名生成sql 而是按照@query的名字
+    // JPQL 查询 使用 entity 名字 pageable 仍然可以使用（混合使用）
+    @Query("SELECT c FROM Contact c WHERE c.status = :status")
+    // native query使用 表中的列名
+    //@Query(value = "SELECT * FROM contact_msg c WHERE c.status = :status",nativeQuery = true)
+    // @Param("status") 如果变量名字和query中使用的名字不一样，可以取别名
+    Page<Contact> findByStatus(@Param("status") String status, Pageable pageable);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Contact c SET c.status = ?1 WHERE c.contactId = ?2")
+    int updateStatusById(String status, int id);
+
+    Page<Contact> findOpenMsgs(@Param("status") String status, Pageable pageable);
+
+    @Transactional
+    @Modifying
+    int updateMsgStatus(String status, int id);
+
+    @Query(nativeQuery = true)
+    Page<Contact> findOpenMsgsNative(@Param("status") String status, Pageable pageable);
+
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true)
+    int updateMsgStatusNative(String status, int id);
 
 }
